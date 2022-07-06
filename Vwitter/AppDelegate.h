cfffc3eec37d7f5df8c08d60e4f8d26fb57e699d
Cas Li <tina.li003@gmail.com>
2022-07-06 14:47:06 -0700

    first commit

---
+++ Vwitter/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Vwitter
+//
+//  Created by Christina Li on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Vwitter/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Vwitter
+//
+//  Created by Christina Li on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
