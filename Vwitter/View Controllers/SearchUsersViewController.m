d85d7baa2378c3d4dd0530aa66b4932c97b83e53
Cas Li <tina.li003@gmail.com>
2022-07-21 14:05:33 -0700

    refactored search users into one function

--- Vwitter/View Controllers/SearchUsersViewController.m
+++ Vwitter/View Controllers/SearchUsersViewController.m
@@ -17,8 +17,6 @@
 @interface SearchUsersViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UserCellDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
-@property (strong, nonatomic) NSMutableArray<VWUser *> *arrayOfUsers;
-@property (strong, nonatomic) NSArray<VWUser *> *filteredSearchResults;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (strong, nonatomic) NSMutableArray<UserCellViewModel *> *arrayOfUserCellViewModels;
 @property (nonatomic, readwrite) int requestCount;
@@ -29,82 +27,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
     self.arrayOfUserCellViewModels = [[NSMutableArray alloc] init];
-    [self loadData];
+    [self loadSearchResults:nil];
     
     self.searchBar.delegate = self;
-    self.filteredSearchResults = self.arrayOfUsers;
     
 }
 
-- (void)loadData {
-    __weak typeof(self) weakSelf = self;
-    [PFCloud callFunctionInBackground:@"fetchUserCellData"
-                       withParameters:@{@"limit":@20, @"currentUserId":[VWUser currentUser].objectId, @"searchString":@""}
-                                block:^(id results, NSError *error) {
-        typeof(self) strongSelf = weakSelf;
-        if (!strongSelf) {
-            NSLog(@"I got killed!");
-            return;
-        }
-        if (!error) {
-          NSLog(@"%@", results);
-          NSArray *_Nullable resultsArray = CAST_TO_CLASS_OR_NIL(results, NSArray);
-            if (!resultsArray) {
-                return;
-            }
-          for (id object in resultsArray) {
-              NSDictionary *_Nullable dictionary = CAST_TO_CLASS_OR_NIL(object, NSDictionary);
-              if (!dictionary) {
-                NSLog(@"not a dictionary");
-                continue;
-              }
-              VWUser *_Nullable currentUser = CAST_TO_CLASS_OR_NIL(object[@"user"], VWUser);
-              if (!currentUser) {
-                  NSLog(@"not a user");
-                  continue;
-              }
-              NSNumber *_Nullable isFollowing = CAST_TO_CLASS_OR_NIL(object[@"isFollowing"], NSNumber);
-              if (!isFollowing) {
-                  NSLog(@"not a number");
-                  continue;
-              }
-              UserCellViewModel *newUCVW = [[UserCellViewModel alloc] initWithUser:currentUser withUserId:currentUser.objectId withUsername:currentUser.username withIsFollowing:isFollowing.boolValue];
-              [strongSelf.arrayOfUserCellViewModels addObject:newUCVW];
-          }
-        
-          [strongSelf.tableView reloadData];
-          
-        }
-      else {
-          NSLog(@"there was an error, u suck");
-      }
-    }];
-    
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return self.arrayOfUserCellViewModels.count;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UserCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UserCell" forIndexPath:indexPath];
-    cell.userCellViewModel = self.arrayOfUserCellViewModels[indexPath.row];
-    cell.delegate = self;
-
-    return cell;
-}
-
-- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+- (void)loadSearchResults:(NSString *_Nullable)searchString {
     const int currentRequestCount = self.requestCount++;
-    
     __weak typeof(self) weakSelf = self;
+    NSMutableDictionary *params = @{@"limit":@20, @"currentUserId":[VWUser currentUser].objectId}.mutableCopy;
+    if (searchString && ![searchString isEqualToString:@""]) {
+        params[@"searchString"] = searchString;
+    }
     [PFCloud callFunctionInBackground:@"fetchUserCellData"
-                       withParameters:@{@"limit":@20, @"currentUserID":[VWUser currentUser].objectId, @"searchString":searchText}
+                       withParameters:params
                                 block:^(id results, NSError *error) {
         typeof(self) strongSelf = weakSelf;
         if (!strongSelf) {
@@ -116,7 +58,7 @@
             return;
         }
         if (!error) {
-          NSLog(@"%@", results);
+            NSLog(@"%@", results);
             NSArray *_Nullable resultsArray = CAST_TO_CLASS_OR_NIL(results, NSArray);
             if (!resultsArray) {
                 return;
@@ -125,8 +67,8 @@
             for (id object in resultsArray) {
                 NSDictionary *_Nullable dictionary = CAST_TO_CLASS_OR_NIL(object, NSDictionary);
                 if (!dictionary) {
-                  NSLog(@"not a dictionary");
-                  continue;
+                    NSLog(@"not a dictionary");
+                    continue;
                 }
                 VWUser *_Nullable currentUser = CAST_TO_CLASS_OR_NIL(object[@"user"], VWUser);
                 if (!currentUser) {
@@ -141,15 +83,31 @@
                 UserCellViewModel *newUCVW = [[UserCellViewModel alloc] initWithUser:currentUser withUserId:currentUser.objectId withUsername:currentUser.username withIsFollowing:isFollowing.boolValue];
                 [strongSelf.arrayOfUserCellViewModels addObject:newUCVW];
             }
-        
-          [strongSelf.tableView reloadData];
-          
+            
+            [strongSelf.tableView reloadData];
+            
         }
-      else {
-          NSLog(@"there was an error, u suck");
-      }
+        else {
+            NSLog(@"there was an error, u suck");
+        }
     }];
+    
+}
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.arrayOfUserCellViewModels.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UserCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UserCell" forIndexPath:indexPath];
+    cell.userCellViewModel = self.arrayOfUserCellViewModels[indexPath.row];
+    cell.delegate = self;
+    
+    return cell;
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    [self loadSearchResults:searchText];
 }
 
 - (void)didFollowUserWithViewModel:(UserCellViewModel *)viewModel {
